60ca730f665e42e8456de903462971322e6f2f8e
Torsten Becker <torsten.becker@gmail.com>
2009-03-23 20:50:54 +0200

    Customize should work now.

--- customize.rb
+++ customize.rb
@@ -1,8 +1,32 @@
 #!/usr/bin/env ruby
 # Customizes the files of sandboxed-safari to work with your setup.
+# Copyright (C) 2009 Torsten Becker <torsten.becker@gmail.com>
 
-puts ENV['HOME']
+policy_file    = 'safari-policy.sb'
+wrapper_script = 'sandboxed-safari.sh'
 
+home = ENV['HOME']
+raise "Can't detect home-directory"  if home.nil?
+
+ss_path = File.expand_path(File.dirname(__FILE__))
+
+
+patched_policy = File.open(policy_file, 'rb') do |file|
+  file.readlines.join.gsub(/HOME/, home)
+end
+
+patched_wrapper = File.open(wrapper_script, 'rb') do |file|
+  file.readlines.join.gsub(/SS_PATH/, ss_path)
+end
+
+
+File.open(policy_file, 'wb') do |file|
+  file.write patched_policy
+end
+
+File.open(wrapper_script, 'wb') do |file|
+  file.write patched_wrapper
+end
 
 
 next_steps = <<EOM
